884e03deb1ded8b092f6145aa67d066ce81ff183
matt-russo <russomatt00@gmail.com>
2021-11-01 12:01:49 -0400

    fixed typo with p command

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ function init(){
 
    document.body.onkeydown = function(e){
      //console.log(e.keyCode + " pressed");
-     if(e.keyCode == 32 || e.keyCode == 880){ // space bar or p
+     if(e.keyCode == 32 || e.keyCode == 88){ // space bar or p
        if(settings.play){
          settings.play = false;
          handleStop();
